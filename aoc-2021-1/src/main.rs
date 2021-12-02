353e70790d96c1eb2d312463f68e1ab91086421f
Raphael Nestler <raphael.nestler@sensirion.com>
2021-12-02 13:45:35 +0100

    Day 1 part 2

--- aoc-2021-1/src/main.rs
+++ aoc-2021-1/src/main.rs
@@ -11,6 +11,11 @@ fn part_1(input: &Vec<i32>) -> usize {
     input.iter().zip(input[1..].iter()).filter(|(a,b)| b > a).count()
 }
 
+fn part_2(input: &Vec<i32>) -> usize {
+    let averages: Vec<i32> = input.windows(3).map(|a| a.iter().sum()).collect();
+    return part_1(&averages)
+}
+
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut file = File::open("input.txt")?;
@@ -20,8 +25,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input = parse(&contents);
 
     let solution_1 = part_1(&input);
-
     println!("Part 1: {}", solution_1);
+
+    let solution_2 = part_2(&input);
+    println!("Part 2: {}", solution_2);
 
     Ok(())
 }
